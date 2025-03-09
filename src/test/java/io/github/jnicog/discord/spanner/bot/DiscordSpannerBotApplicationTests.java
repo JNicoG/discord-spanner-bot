ea4c0a49ed76497d081db5ed635b0fa2300f466a
Nico Ganacias <113068462+JNicoG@users.noreply.github.com>
2025-03-09 00:51:48 +0000

    Init

---
+++ src/test/java/io/github/jnicog/discord/spanner/bot/DiscordSpannerBotApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.jnicog.discord.spanner.bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscordSpannerBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
