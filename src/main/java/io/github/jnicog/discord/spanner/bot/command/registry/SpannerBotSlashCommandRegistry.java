3dd7e753386009b61963e6d6c4948f2151cfaf2e
JNicoG <113068462+JNicoG@users.noreply.github.com>
2026-02-17 22:00:21 +0000

    set slash commands guild only

--- src/main/java/io/github/jnicog/discord/spanner/bot/command/registry/SpannerBotSlashCommandRegistry.java
+++ src/main/java/io/github/jnicog/discord/spanner/bot/command/registry/SpannerBotSlashCommandRegistry.java
@@ -1,5 +1,6 @@
 package io.github.jnicog.discord.spanner.bot.command.registry;
 
+import net.dv8tion.jda.api.interactions.InteractionContextType;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.CommandData;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
@@ -12,13 +13,19 @@ public class SpannerBotSlashCommandRegistry implements SlashCommandRegistry {
     @Override
     public List<CommandData> getCommands() {
         return List.of(
-                Commands.slash("k", "Join the queue"),
-                Commands.slash("keen", "Join the queue"),
-                Commands.slash("unkeen", "Leave the queue"),
-                Commands.slash("keeners", "Show current queue members"),
+                Commands.slash("k", "Join the queue")
+                        .setContexts(InteractionContextType.GUILD),
+                Commands.slash("keen", "Join the queue")
+                        .setContexts(InteractionContextType.GUILD),
+                Commands.slash("unkeen", "Leave the queue")
+                        .setContexts(InteractionContextType.GUILD),
+                Commands.slash("keeners", "Show current queue members")
+                    .setContexts(InteractionContextType.GUILD),
                 Commands.slash("spanners", "Display the number of spanners a user has accumulated")
+                        .setContexts(InteractionContextType.GUILD)
                         .addOption(OptionType.USER, "user", "The user to perform a lookup against.", false),
                 Commands.slash("leaderboard", "Display the leaderboard for this message channel")
+                        .setContexts(InteractionContextType.GUILD)
         );
     }
 }
